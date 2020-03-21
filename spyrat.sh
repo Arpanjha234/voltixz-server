d95c7843a9a5483903d4f8a6e17c80c3d8d9fb29
Arpanjha234 <62328473+Arpanjha234@users.noreply.github.com>
2020-03-21 16:11:58 +0530

    Update spyrat.sh

--- spyrat.sh
+++ spyrat.sh
@@ -1,38 +1,54 @@
-
+#!/bin/sh
 
 printf " \e[8;00m  ____  ______   __  ____      _  _____ \e[0m\n"
+
 printf " \e[8;00m / ___||  _ \ \ / / |  _ \    / \|_   _| \e[0m\n"
+
 printf " \e[8;00m \___ \| |_) \ V /  | |_) |  / _ \ | |  \e[0m\n"
+
 printf " \e[8;00m  ___) |  __/ | |   |  _ <  / ___ \| |  \e[0m\n"
+
 printf " \e[8;00m |____/|_|    |_|   |_| \_\/_/   \_\_|     \e[0m\n"                                
+
 printf "\n"
+
 printf " \e[1;77m v1.0 CODED BY ARPANJHA \e[0m \n"
+
 printf "\n"
+
 printf "\n"
+
 printf " (1) START EXPLOIT \e[0m \n"
+
 printf "\n"
+
 printf " (2) GRAB SMS \e[0m \n"
+
 printf "\n"
+
 printf " (3) GRAB CONTACT \e[0m \n"
+
 printf "\n"
+
 printf " (4) GRAB FONTCAM SNAP \e[0m \n"
+
 printf "\n" 
+
 printf " (5) GRAB CALL LOGS \e[0m \n"
+
 printf "\n" 
+
 printf "(6) GRAB TALK (RECORD VOICE)
 
 #CHECKING USER INPUT
 
 read input
+
 if [ $input == 1]
+
 then
-echo "starting exploit"
+
+print "starting exploit"
+
 fi
-
-
-
-
-
-
-
 
